1702288db4aafde5a6fede8b32d2c26663e6af93
Adam Tilly <tillyjadam@gmail.com>
2022-01-22 21:23:50 -0600

    working on header, renamed home to page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import {
   ApolloClient,
@@ -11,7 +12,7 @@ import { setContext } from '@apollo/client/link/context';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// import Home from './pages/Home';
+// import Home from './pages/Game';
 // import Login from './pages/Login';
 // import NoMatch from './pages/NoMatch';
 // import SingleThought from './pages/SingleThought';
@@ -42,20 +43,18 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div className="flex-column justify-flex-start min-100-vh">
-          <h1> test</h1>
           <Header />
           <div className="container">
             {/* <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
+              <Route exact path="/game" component={Game} />
+              <Route exact path="/" component={Login} />
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/profile/:username?" component={Profile} />
-              <Route exact path="/thought/:id" component={SingleThought} />
 
               <Route component={NoMatch} />
             </Switch> */}
           </div>
-          <Footer />
+          {/* <Footer /> */}
         </div>
       </Router>
     </ApolloProvider>
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// import Auth from '../../utils/auth';
-
 const Header = () => {
   const logout = event => {
     event.preventDefault();
@@ -11,24 +9,24 @@ const Header = () => {
 
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <Link to="/">
-          <h1>Connect-4</h1>
-        </Link>
-
-        <nav className="text-center">
-        {/* {Auth.loggedIn() ? ( */}(
-            <>
-              <Link to="/profile">Me</Link>
-              <a href="/" onClick={logout}>
-                Logout
-              </a>
-            </>
+      <div className="container navbar justify-space-between-lg ">
+        <div className='container'>
+        <img src='../../assets/android-chrome-192x192.png' alt="Connect-4 logo" className='header-img'>
+        </img><p className='header-title align-middle '>Connect-4</p>
+        </div>
+        <nav className="text-center justify-space-between-lg">
+          {/* {Auth.loggedIn() ? ( */}(
+          <>
+            <Link to="/profile">Me</Link>
+            <a href="/" onClick={logout}>
+              Logout
+            </a>
+          </>
           ) : (
-            <>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-            </>
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </>
           ){/* )} */}
         </nav>
       </div>
