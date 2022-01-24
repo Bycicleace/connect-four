0897691bf46403430611b12e21bd1282e12c449b
Ian Holmes <concord511@gmail.com>
2022-01-24 09:36:46 -0600

    added auth.js to utility folder for signing jwt

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,14 @@
 const { User, Game } = require('../models');
-// const { AuthenticationError } = require('apollo-server-express');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
         game: async (parent, { gameId }) => {
             return Game.findOne(gameId);
+        },
+        user: async (parent, { username }) => {
+            return User.findOne(username);
         }
     },
 
@@ -13,6 +17,13 @@ const resolvers = {
             const game = await Game.create(args);
 
             return game;
+        },
+
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return user;
         }
     }
 };
