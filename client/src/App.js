8edce09f73d3aecf73d61123101864c0d35b028d
Adam Tilly <tillyjadam@gmail.com>
2022-01-23 21:39:49 -0600

    updated app.js with auth, added login_user and add_user mutations, created login and sign up pages.:

--- client/src/App.js
+++ client/src/App.js
@@ -12,12 +12,10 @@ import { setContext } from '@apollo/client/link/context';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// import Home from './pages/Game';
-// import Login from './pages/Login';
-// import NoMatch from './pages/NoMatch';
-// import SingleThought from './pages/SingleThought';
-// import Profile from './pages/Profile';
-// import Signup from './pages/Signup';
+import Game from './pages/Game';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -45,14 +43,12 @@ function App() {
         <div className="flex-column justify-flex-start min-100-vh">
           <Header />
           <div className="container">
-            {/* <Switch>
+            <Switch>
               <Route exact path="/game" component={Game} />
               <Route exact path="/" component={Login} />
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/profile/:username?" component={Profile} />
-
-              <Route component={NoMatch} />
-            </Switch> */}
+            </Switch>
           </div>
           {/* <Footer /> */}
         </div>
@@ -60,6 +56,8 @@ function App() {
     </ApolloProvider>
   );
 }
+
+
 
 export default App;
 
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../assets/android-chrome-192x192.png';
+import Auth from '../../utils/auth'
 
 const Header = () => {
   const logout = event => {
     event.preventDefault();
-    // Auth.logout();
+    Auth.logout();
   };
 
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
-      <div className="container navbar justify-space-between-lg ">
-        <div className='container'>
-        <img src='../../assets/android-chrome-192x192.png' alt="Connect-4 logo" className='header-img'>
-        </img><p className='header-title align-middle '>Connect-4</p>
+      <div className="row navbar justify-space-between-lg ">
+        <div className='col-6 justify-space-between'>
+          <img src={Logo} alt="Connect-4 logo" className='header-img'></img>
+          <h1 className='header-title' style={{ display: "inline-block" }}>Connect-4</h1>
         </div>
-        <nav className="text-center justify-space-between-lg">
-          {/* {Auth.loggedIn() ? ( */}(
-          <>
-            <Link to="/profile">Me</Link>
-            <a href="/" onClick={logout}>
-              Logout
-            </a>
-          </>
+        <nav className="col-6">
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
           ) : (
-          <>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </>
-          ){/* )} */}
+            <>
+              <Link to="/" className='header-link'>Login</Link>
+              <Link to="/signup"className='header-link'>Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
