309dfa6ec854bbdf98906ba585b7a1c51ab24e02
aziz <ahmed613@umn.edu>
2022-01-26 16:35:27 -0600

    update queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -6,10 +6,31 @@ export const QUERY_USERS = gql`
       _id
       username
       email
+      wins
       games {
         _id
         player1
-        player2 
+        player2
+        playerTurn
+        board
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($id: ID!) {
+    user(_id: $id) {
+      _id
+      username
+      email
+      wins
+      games {
+        _id
+        player1
+        player2
+        playerTurn
+        board
+      }
     }
   }
 `;
@@ -25,7 +46,6 @@ export const QUERY_GAMES = gql`
   }
 `;
 
-
 export const QUERY_GAME = gql`
   query game($id: ID!) {
     game(_id: $id) {
@@ -34,6 +54,11 @@ export const QUERY_GAME = gql`
       player2
       playerTurn
       board
+      }
+    }
   }
 `;
+
+
+
 
