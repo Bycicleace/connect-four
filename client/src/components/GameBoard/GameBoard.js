1fe9609f9e02e1dedab9999d358784c71bbd65eb
kvammee <elliott.kvamme@gmail.com>
2022-01-28 12:38:27 -0600

    Created GameBoard component

--- client/src/components/GameBoard/GameBoard.js
+++ client/src/components/GameBoard/GameBoard.js
@@ -3,7 +3,10 @@ import { useQuery } from 'react-router-dom';
 import { QUERY_GAME } from '../../utils/queries';
 
 function GameBoard(props) {
+    // Get game ID from prop passing
     const { gameId } = props;
+
+    // Get the board from the query
     const { loading, data } = useQuery(QUERY_GAME, {
         variables: { id: gameId }
     });
@@ -19,7 +22,13 @@ function GameBoard(props) {
 
     return (
         <div className='gameBoard__container'>
-
+            {board.forEach(column => {
+                <div className='gameBoard__column'>
+                    {column.split('').forEach(field => {
+                        <img id={`gameBoard__${column}${field}`} className='gameBoard__field' src={`../../assets/board/${field}.png`} />
+                    })}
+                </div>
+            })}
         </div>
     );
 };
