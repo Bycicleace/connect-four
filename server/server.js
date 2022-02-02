0279c63c5b47337f82b54a171725fd82027f611e
kvammee <elliott.kvamme@gmail.com>
2022-02-01 19:55:49 -0600

    Adjusted for live

--- server/server.js
+++ server/server.js
@@ -41,9 +41,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // UNCOMMENT FOR LIVE
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
